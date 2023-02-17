9bd4917d37f803b2f8fa72e8e66b37c9c0b60f33
mostafa <mostafarohanii0@gmail.com>
2023-02-17 17:26:16 +0330

    change the paddings and gaps

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,7 +20,7 @@ const Home = () => {
              dark:from-[rgba(0,29,36,1)] dark:via-[rgba(4,43,54,1)] dark:to-[#0b4457]
             ">
 
-            <div className="md:w-[90%] min-h-[90vh] lg:w-[75%] md:rounded-2xl m-auto md:shadow md:shadow-white px-3 pb-3 md:px-4 lg:px-5">
+            <div className="md:w-[90%] min-h-[90vh] lg:w-[75%] md:rounded-2xl m-auto md:shadow md:shadow-white px-3 pb-5 md:px-4 lg:px-5">
                 <Header />
                 <Search setLatAndLon={setLatAndLon} />
                 {latAndLon ? <Weather latAndLon={latAndLon} /> : <Empty />}
